42c27b0317161e1e6588b1b45679535baecf2f3a
Jose <jose@octomedia.mx>
2016-08-10 22:30:50 +0000

    remove sensitive

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,3 +35,24 @@ set :repo_url, 'https://jose-octomedia:Rockandroll1@github.com/jose-octomedia/ar
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+#remove after first run
+#add on second run
+#remove on later runs
+
+#namespace :deploy do
+ # namespace :assets do
+  #  task :precompile, :roles => :web, :except => { :no_release => true } do
+   ##   begin
+     #   from = source.next_revision(current_revision) # <-- Fail here at first-time deploy because of current/REVISION absence
+     # rescue
+      #  err_no = true
+      #end
+     # if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      #  run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      #else
+       # logger.info "Skipping asset pre-compilation because there were no asset changes"
+     # end
+   #end
+  #end
+#end
